7a1c5921ea6f6efbd6c2d086f9c3f8d6e906146b
Daniele Checchin <danielechecchin26@gmail.com>
2025-08-07 12:24:11 +0200

    slices

--- main.go
+++ main.go
@@ -11,4 +11,10 @@ func main() {
 	names := [3]string{"Alice", "Bob", "Charlie"}
 
 	fmt.Println(names, len(names))
+
+	var numbers = []int{1, 2, 3}
+	numbers[0] = 7
+	numbers = append(numbers, 8, 9)
+
+	fmt.Println(numbers, len(numbers))
 }
