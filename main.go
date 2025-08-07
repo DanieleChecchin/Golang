b9fe5c8f8d423a9352181ac545db5bf2bd8f2d2c
Daniele Checchin <danielechecchin26@gmail.com>
2025-08-07 11:18:38 +0200

    fmt - Print

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import "fmt"
 
 func main() {
 
-	var myName string = "Daniele"
-	var mySurname = "Checchin"
+	// var myName string = "Daniele"
+	// var mySurname = "Checchin"
 
-	//var age int = 28
-	age := 28
+	// //var age int = 28
+	// age := 28
 
-	//var height float64 = 1.71
-	height := 1.71
+	// //var height float64 = 1.71
+	// height := 1.71
 
-	fmt.Println(myName, mySurname, age, height)
+	// fmt.Println(myName, mySurname, age, height)
 
-	myphrase := "My name is Daniele and i'm learning to program in Go!"
+	// myphrase := "My name is Daniele and i'm learning to program in Go!"
 
-	fmt.Println(myphrase)
+	// fmt.Println(myphrase)
+
+	// fmt.Print("Hello, ")
+	// fmt.Print("World!")
+
+	fmt.Print("Hello, \n")
+	fmt.Print("World! ")
 }
