e8c7431e121caaabd9877074a6424c7fb259fc1b
Daniele Checchin <danielechecchin26@gmail.com>
2025-08-07 10:36:25 +0200

    variables - numbers

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
+
 	var myName string = "Daniele"
 	var mySurname = "Checchin"
-	var example string
 
-	fmt.Println(myName, mySurname, example)
+	//var age int = 28
+	age := 28
+
+	//var height float64 = 1.71
+	height := 1.71
+
+	fmt.Println(myName, mySurname, age, height)
 
 	myphrase := "My name is Daniele and i'm learning to program in Go!"
 
